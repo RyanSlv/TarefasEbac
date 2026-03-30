369ee0962e9546392e53f83771c7e7a11c2916e1
RyanSlv <ryan.silva7824@gmail.com>
2026-03-30 13:47:10 -0300

    corrigindo mod 17

--- MOD17/TarefaMod17/src/br/com/ryans/tarefa/ListaCarros.java
+++ MOD17/TarefaMod17/src/br/com/ryans/tarefa/ListaCarros.java
@@ -10,36 +10,33 @@ public class ListaCarros {
 
     public static void main(String[] args) {
 
-        List<BugattiChiron> carro = new ArrayList<>();
+        List<Carro> carros = new ArrayList<>();
+
         BugattiChiron BChi = new BugattiChiron();
         BChi.setMarca("Bugatti");
         BChi.setCor("Azul");
         BChi.setCambio("Automático");
         BChi.setModelo("Chiron");
-        carro.add(BChi);
-        listaBugatti(carro);
+        carros.add(BChi);
 
-        List<FerrariF430> carro2 = new ArrayList<>();
         FerrariF430 FF430 = new FerrariF430();
         FF430.setMarca("Ferrari");
         FF430.setCor("Vermelho");
         FF430.setCambio("Manual");
         FF430.setModelo("F430");
-        carro2.add(FF430);
-        listaFerrari(carro2);
+        carros.add(FF430);
 
-
+        listaCarros(carros);
     }
+    private static void listaCarros(List<? extends Carro> carros) {
+        for (Carro c : carros) {
+            if (c instanceof FerrariF430) {
+                System.out.println(((FerrariF430) c).imprimirListaF430());
+            } else if (c instanceof BugattiChiron) {
+                System.out.println(((BugattiChiron) c).imprimirListaChiron());
 
-    private static void listaFerrari(List<? extends FerrariF430> carro) {
-        for (FerrariF430 f : carro) {
-            System.out.println(f.imprimirListaF430());
+            }
         }
     }
 
-    private static void listaBugatti(List<? extends BugattiChiron> carro1) {
-        for (BugattiChiron b : carro1) {
-            System.out.println(b.imprimirListaChiron());
-        }
-    }
 }
