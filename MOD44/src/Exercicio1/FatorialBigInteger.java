fab2d7c50a2d8f7e413b2491ca976cea86a639eb
RyanSlv <ryan.silva7824@gmail.com>
2026-09-25 12:53:18 -0300

    Corrigindo erros

--- MOD44/src/Exercicio1/FatorialBigInteger.java
+++ MOD44/src/Exercicio1/FatorialBigInteger.java
@@ -5,11 +5,9 @@ import java.math.BigInteger;
 public class FatorialBigInteger {
 
     public static BigInteger calcularFatorial(BigInteger n) {
-        //Condição de parada: 0! e 1! = 1
         if (n.equals(BigInteger.ZERO) || n.equals(BigInteger.ONE)) {
             return BigInteger.ONE;
         }
-        //Chamada recursiva
         return n.multiply(calcularFatorial(n.subtract(BigInteger.ONE)));
     }
 
